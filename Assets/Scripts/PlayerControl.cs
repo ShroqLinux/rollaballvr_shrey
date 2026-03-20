556418c422909bc5adf1202685cdf6bd5c9195c2
Pethkar <shp5721@thi.de>
2026-03-20 15:54:16 +0100

    Connection to VR is functional

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -7,6 +7,7 @@ public class PlayerControl : MonoBehaviour
     float movementX;
     float movementY;
     public float speed;
+    public bool keyboard;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,14 +22,26 @@ public class PlayerControl : MonoBehaviour
         movementY = movementVector.y;
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+
+
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 movement = new Vector3();
-        movement.x = movementX;
-        movement.y = 0f;
-        movement.z = movementY;
-        movement = movement * speed;
-        rb.AddForce(movement);
+        if (keyboard)
+        {
+            Vector3 movement = new Vector3();
+            movement.x = movementX;
+            movement.y = 0f;
+            movement.z = movementY;
+            movement = movement * speed;
+            rb.AddForce(movement);
+        }
     }
 }
